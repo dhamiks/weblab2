dba669040035ecd4f997666f3b2aad7fd545d9e8
dhamiks <hamiks33@gmail.com>
2025-01-03 14:02:01 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Piano from "./piano";
+function App() {
+  return (
+    <div className="app-container">
+      <Piano />
+    </div>
+  );
+}
+
+export default App;
